62b73977dc13782bc19d6a168953457a5c6bc7c3
HOELCODE <hcostantini1@gmail.com>
2025-04-16 15:02:58 +0200

    Ajout au local storage lors du submit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import '../css/Home.css';
 
 import states from '../data/Data.js';
 
 const Home = () => {
+
+    const [employee, setEmployee] = useState({
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        startDate: '',
+        street: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        department: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEmployee({
+            ...employee,
+            [name]: value
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('employee', JSON.stringify(employee));
+    }
+
     return (
         <div className="home-container">
             <h1>HRnet</h1>
@@ -12,44 +38,44 @@ const Home = () => {
             </Link>
             <h2>Create Employee</h2>
             <div className="htmlForm-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="FirstName">First Name</label>
-                    <input type="text" name="FirstName"></input>
+                    <input type="text" name="FirstName" onChange={handleChange}></input>
 
                     <label htmlFor="LastName">Last Name</label>
-                    <input type="text" name="LastName"></input>
+                    <input type="text" name="LastName" onChange={handleChange}></input>
 
                     <label htmlFor="BirthDate">Date of Birth</label>
-                    <input type="date" name="BirthDate"></input>
+                    <input type="date" name="BirthDate" onChange={handleChange}></input>
 
                     <label htmlFor="StartDate">Start Date</label>
-                    <input type="date" name="StartDate"></input>
+                    <input type="date" name="StartDate" onChange={handleChange}></input>
 
                     <div className="separateur"></div>
 
                     <div className="adress-container">
                         <span>Address</span>
                         <label htmlFor="Street">Street</label>
-                        <input type="text" name="Street"></input>
+                        <input type="text" name="Street" onChange={handleChange}></input>
 
                         <label htmlFor="City">City</label>
-                        <input type="text" name="City"></input>
+                        <input type="text" name="City" onChange={handleChange}></input>
 
                         <label htmlFor="State">State</label>
-                        <select name="State" id="State">
+                        <select name="State" id="State" onChange={handleChange}>
                             {states.map((state, index) => (
                                 <option key={index} value={state.abbreviation}>{state.name}</option>
                             ))}
                         </select>
 
                         <label htmlFor="ZipCode">Zip Code</label>
-                        <input type="number" name="ZipCode"></input>
+                        <input type="number" name="ZipCode" onChange={handleChange}></input>
                     </div>
 
                     <div className="separateur"></div>
 
                     <label htmlFor="department">Departement</label>
-                    <select name="department" id="department">
+                    <select name="department" id="department" onChange={handleChange}>
                         <option>Sales</option>
                         <option>Marketing</option>
                         <option>Engineering</option>
