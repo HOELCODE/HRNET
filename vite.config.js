495dd486a16ef919a205a3238978fb62a10c4f80
HOELCODE <hcostantini1@gmail.com>
2025-04-24 15:24:25 +0200

    modifs vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/HRNET/', // ← Mets ici le nom EXACT de ton repo GitHub
   plugins: [react()],
 })
